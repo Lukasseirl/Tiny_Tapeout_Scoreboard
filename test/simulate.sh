fe2f8896a91d2efcc751b4ae1bd79902bf004215
Lukasseirl <147480159+Lukasseirl@users.noreply.github.com>
2025-10-27 11:15:43 +0100

    Update simulate.sh

--- test/simulate.sh
+++ test/simulate.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # =====================================================
-# Author: Simon Dorrer
+# Author: Simon Dorrer  
 # Last Modified: 02.10.2025
-# Description: This .sh file verifies and simulates a verilog testbench with Verilator, IVerilog and GTKWave.
+# Description: Adapted for scoreboard testbench
 # =====================================================
 
 set -e -x
@@ -15,24 +15,32 @@ NC='\033[0m'
 
 name=$1
 
-RTL=${RTL:-../src}
-SRC_FOLDER=${SRC_FOLDER:-.}
+RTL=${RTL:-../src}           # Source files location
+TEST_FOLDER=${TEST_FOLDER:-.} # Testbench location
 
 echo -e "${GREEN}Verilator:------------------------------------------ ${NC}"
-verilator --lint-only -I"$RTL" "$RTL"/"$name".v # -I"$RTL" for multiple Verilog file dependencies in "src" folder
+verilator --lint-only -I"$RTL" "$TEST_FOLDER"/"$name"_tb.v
 
 echo -e "${GREEN}IVerilog:------------------------------------------- ${NC}"
-iverilog -g2005 -I"$RTL" "$RTL/$name.v" "$SRC_FOLDER/${name}_tb.v" # -I"$RTL" for multiple Verilog file dependencies in "src" folder
+# Kompiliert alle notwendigen Files
+iverilog -g2005 -I"$RTL" \
+  "$RTL/tt_um_scoreboard_simple_top.v" \
+  "$RTL/scoreboard_simple_controller.v" \
+  "$RTL/display_simple_controller.v" \
+  "$RTL/button_debouncer.v" \
+  "$RTL/long_press_detector.v" \
+  "$RTL/seven_segment_decoder.v" \
+  "$TEST_FOLDER/${name}_tb.v"
 
-echo -e "${GREEN}a:-------------------------------------------------- ${NC}"
+echo -e "${GREEN}Simulation:----------------------------------------- ${NC}"
 ./a.out
 
 echo -e "${GREEN}GTKWave:-------------------------------------------- ${NC}"
-if [ -e "$SRC_FOLDER"/"$name"_tb.gtkw ]
+if [ -e "$TEST_FOLDER"/"$name"_tb.gtkw ]
 then
-  gtkwave "$SRC_FOLDER"/"$name"_tb.gtkw
+  gtkwave "$TEST_FOLDER"/"$name"_tb.gtkw
 else
-  gtkwave "$SRC_FOLDER"/"$name"_tb.vcd
+  gtkwave "$TEST_FOLDER"/"$name"_tb.vcd
 fi
 
 # Clean
