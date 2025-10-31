f15804558a3b72f5b4db0c6a4dbcbdb9d2c055d2
Lukasseirl <147480159+Lukasseirl@users.noreply.github.com>
2025-10-31 10:12:16 +0100

    Update simulate.sh

--- test/simulate.sh
+++ test/simulate.sh
@@ -16,12 +16,11 @@ verilator --lint-only --timing -I"$RTL" "$TEST_FOLDER/${name}_tb.v"
 
 echo -e "${GREEN}IVerilog:------------------------------------------- ${NC}"
 iverilog -g2005 -I"$RTL" \
-  "$RTL/tt_um_scoreboard_simple_top.v" \
-  "$RTL/scoreboard_simple_controller.v" \
-  "$RTL/display_simple_controller.v" \
-  "$RTL/button_debouncer.v" \
-  "$RTL/long_press_detector.v" \
-  "$RTL/seven_segment_decoder.v" \
+  "$RTL/top_level.v" \
+  "$RTL/pushbutton_processor.v" \
+  "$RTL/counter_v2.v" \
+  "$RTL/bin_to_decimal.v" \
+  "$RTL/dual_7_seg.v" \
   "$TEST_FOLDER/${name}_tb.v"
 
 echo -e "${GREEN}Simulation:----------------------------------------- ${NC}"
