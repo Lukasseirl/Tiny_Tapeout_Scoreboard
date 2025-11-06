0200792e4eb52e44434410807c054f0ffe0f1861
Lukasseirl <147480159+Lukasseirl@users.noreply.github.com>
2025-11-06 17:01:22 +0100

    Update bin_to_decimal_tb.v

--- test/bin_to_decimal_tb.v
+++ test/bin_to_decimal_tb.v
@@ -2,6 +2,46 @@
 `include "../src/bin_to_decimal.v"
 
 module tb_bin_to_decimal;
+    reg [6:0] bin_i;
+    wire [3:0] tens_o, ones_o;
+    
+    // Instanziere das Modul
+    bin_to_decimal dut (
+        .bin_i(bin_i),
+        .ones_o(ones_o),
+        .tens_o(tens_o)
+    );
+    
+    
+    initial begin
+        $dumpfile("bin_to_decimal_tb.vcd");
+        $dumpvars;
+        
+        bin_i = 7'd0;
+        
+        
+        // Testfälle
+        bin_i = 7'd0;   #1500; // Sollte 0 und 0 ausgeben
+        bin_i = 7'd5;   #1500; // Sollte 0 und 5 ausgeben
+        bin_i = 7'd15;  #1500; // Sollte 1 und 5 ausgeben
+        bin_i = 7'd42;  #1500; // Sollte 4 und 2 ausgeben
+        
+        // Weitere Tests nach Reset
+        bin_i = 7'd73;  #1500; // Sollte 7 und 3 ausgeben
+        bin_i = 7'd99;  #1500; // Sollte 9 und 9 ausgeben
+        
+        #10;
+        $finish;
+    end
+    
+endmodule
+
+/* Test for OLD Bin to Dec
+
+`timescale 1ns / 1ns // 'timescale <time_unit> / <time_precision>
+`include "../src/bin_to_decimal.v"
+
+module tb_bin_to_decimal;
     reg clk_i;
     reg rst_i = 1'b1;
     reg [6:0] bin_i;
@@ -53,3 +93,4 @@ module tb_bin_to_decimal;
     end
     
 endmodule
+*/
