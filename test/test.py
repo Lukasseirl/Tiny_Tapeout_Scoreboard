e0e3daa1c9e263ada134f3c28549d7491fe248fe
Lukasseirl <147480159+Lukasseirl@users.noreply.github.com>
2025-11-07 15:48:25 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -26,14 +26,17 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 
-
+    dut.ui_in.value = 8'b00000001    // set ui_in[0] = 1 to simulate a pushbutton-press
+    await ClockCycles(dut.clk, 50)    // hold press for 50 ms so that it gets through the debounce
+    dut.ui_in.value = 8'b00000000    
+    
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 1)   // after about 3 seconds the display should show the the score of player 1 which should be 01 after the first button press
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    #assert dut.uo_out.value == 50
+    assert dut.uo_out.value == 8'00111111b      // ones should show 0 at 7seg display
+    assert dut.uio_out.value == 8'00000110b    // tens should show 1 at 7seg display
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
